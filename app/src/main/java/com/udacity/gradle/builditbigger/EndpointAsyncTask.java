b2b3a8f7fa759c24f85a16623963feafd2be48ca
durga <durga.gm@gmail.com>
2016-09-24 17:04:25 -0700

    feat:  free app variant display interstitial ads between the main activity and the joke-displaying activity feat: display a loading indicator while the joke is being fetched from the server

--- app/src/main/java/com/udacity/gradle/builditbigger/EndpointAsyncTask.java
+++ app/src/main/java/com/udacity/gradle/builditbigger/EndpointAsyncTask.java
@@ -3,6 +3,7 @@ package com.udacity.gradle.builditbigger;
 import android.content.Context;
 import android.content.Intent;
 import android.os.AsyncTask;
+import android.util.Log;
 
 import com.google.api.client.extensions.android.http.AndroidHttp;
 import com.google.api.client.extensions.android.json.AndroidJsonFactory;
@@ -23,6 +24,14 @@ public class EndpointAsyncTask extends AsyncTask<Context, Void, String> {
     private MyApi myApiService = null;
     Context mContext;
 
+    public  EndpointAsyncTask() {
+    }
+
+    @Override
+    protected void onPreExecute() {
+        super.onPreExecute();
+    }
+
     @Override
     protected String doInBackground(Context... params) {
 
@@ -42,13 +51,13 @@ public class EndpointAsyncTask extends AsyncTask<Context, Void, String> {
         try {
             return myApiService.getJoke().execute().getData();
         } catch (IOException ex) {
-            return ex.getMessage();
+            Log.e(mContext.getResources().getString(R.string.endpointtask), ex.getMessage());
+            return mContext.getResources().getString(R.string.loadjokeerror);
         }
     }
 
     @Override
     protected void onPostExecute(String joke) {
-
         super.onPostExecute(joke);
         String jokeStr = mContext.getResources().getString(R.string.joke);
         Intent intent = new Intent(mContext, DisplayJokeActivity.class);
